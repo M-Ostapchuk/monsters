ed8b04ed50e92df8ff4666ac4e34565e3ad72e53
unknown <you@example.com>
2020-01-10 21:35:53 +0200

    Updates

---
+++ precache-manifest.f4e72f6d1d5d17911afdbbaa1969e639.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d0a022d6ba7e2e3d5c9779d67ebef0",
+    "url": "/github.io/monsters/index.html"
+  },
+  {
+    "revision": "88c509ebb8e6d88f58ea",
+    "url": "/github.io/monsters/static/css/main.e9fc1188.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/github.io/monsters/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/monsters/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88c509ebb8e6d88f58ea",
+    "url": "/github.io/monsters/static/js/main.1bdbfa87.chunk.js"
+  },
+  {
+    "revision": "d01c48335104e09b4606",
+    "url": "/github.io/monsters/static/js/runtime-main.5f079cdb.js"
+  }
+]);
